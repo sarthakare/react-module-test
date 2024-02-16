d44f11e6e81175397152803fd6dd1961ddb47112
sarthak <thakresarthak6@gmail.com>
2024-02-16 15:10:30 +0530

    notes with heading completed.

--- src/App.js
+++ src/App.js
@@ -5,6 +5,23 @@ import { IoMdLock } from "react-icons/io";
 import BackgroundImage from "./Images/background-image.png";
 import "./App.css";
 
+// Function to get the first characters of each word in a string
+function getFirstCharacters(inputString) {
+  // Split the string into an array of words
+  const words = inputString.split(" ");
+
+  // Map over the array of words and get the first character of each word
+  const firstCharacters = words.map((word) => word[0].toUpperCase());
+
+  // Join the characters into a string
+  const concatenatedString = firstCharacters.join("");
+
+  // Return the first two characters of the concatenated string
+  return concatenatedString.substring(0, 2);
+}
+
+
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [groups, setGroups] = useState([]);
@@ -23,10 +40,16 @@ const App = () => {
     setNotes([...notes, <Note key={notes.length} />]);
   };
 
-  // Render group list
+  // Render group list with first characters
   const groupList = groups.map((group, index) => (
-    <div key={index} className="group" style={{ backgroundColor: group.color }}>
-      <h3>{group.name}</h3>
+    <div key={index} className="group">
+      <div id="group-color" style={{ backgroundColor: group.color }}>
+        {/* Display the first characters of each word in group.name */}
+        {getFirstCharacters(group.name)}
+      </div>
+      <div id="group-name">
+        <h3>{group.name}</h3>
+      </div>
     </div>
   ));
 
@@ -35,9 +58,7 @@ const App = () => {
       <div id="addNotes">
         <h1>Pocket Notes</h1>
         {/* Render the list of groups */}
-        <div className="group-list">
-          {groupList}
-        </div>
+        <div className="group-list">{groupList}</div>
         <div id="addNoteButtonContainer">
           <AddNoteButton onClick={addNote} />
         </div>
